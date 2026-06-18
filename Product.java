ebf5bfa46b9e0184a2e62aa47e6dbf7bb8507175
Jaideep Reddy Mandadi <mandadijaideep@gmail.com>
2026-06-18 15:25:05 +0530

    Add Product entity with fields and methods

---
+++ Product.java
@@ -0,0 +1,56 @@
+package jaideep.hibernate_maven;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "product")
+public class Product {
+	 @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)  // DB auto-generates the id
+     private int id;
+
+     private String name;
+
+     private double price;
+
+     public Product() {
+     }
+
+     public Product(String name, double price) {
+         this.name = name;
+         this.price = price;
+     }
+
+   
+     public int getId() {
+         return id;
+     }
+
+     public void setId(int id) {
+         this.id = id;
+     }
+
+     public String getName() {
+         return name;
+     }
+
+     public void setName(String name) {
+         this.name = name;
+     }
+
+     public double getPrice() {
+         return price;
+     }
+
+     public void setPrice(double price) {
+         this.price = price;
+     }
+
+     @Override
+     public String toString() {
+         return "Product [id=" + id + ", name=" + name + ", price=" + price + "]";
+     }
+
+}
